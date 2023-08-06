8a31cb177084bcf1bd556d9c59b13ff39e8bdf26
jmulq <james.mulqueeny@outlook.com>
2023-08-06 19:56:25 +0100

    Add basic election contract

---
+++ packages/foundry/contracts/Election.sol
@@ -0,0 +1,57 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "forge-std/console.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Election is Ownable {
+    // Open zeppelin ownable contract
+    // https://docs.openzeppelin.com/contracts/4.x/api/access#Ownable
+
+    bool isVotingOpen = false;
+
+    struct Candidate {
+        string name;
+        string party;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint256 public candidatesCount;
+
+    event VoteCast(address indexed voter, uint256 indexed candidateId);
+    event CandidateAdded(uint256 indexed candidateId);
+    event ElectionResult(uint256 indexed candidateId, uint256 indexed voteCount);
+
+    modifier isElectionActive() {
+        require(isVotingOpen, "Voting is not open");
+        _;
+    }
+
+    constructor() Ownable() {}
+
+    function openVoting() public onlyOwner {
+        isVotingOpen = true;
+    }
+
+    function endVoting() public onlyOwner {
+        isVotingOpen = false;
+    }
+
+    function addCandidate(string memory _name, string memory _party) public onlyOwner {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(_name, _party, 0);
+        emit CandidateAdded(candidatesCount);
+    }
+
+    function vote(uint256 _candidateId) public isElectionActive {
+        require(!voters[msg.sender], "You have already voted");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate");
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+        emit VoteCast(msg.sender, _candidateId);
+    }
+}
---
+++ packages/foundry/test/Election.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../contracts/Election.sol";
+
+contract ElectionTest is Test {
+    Election public election;
+
+    function setUp() public {
+        election = new Election(vm.addr(1));
+    }
+}
