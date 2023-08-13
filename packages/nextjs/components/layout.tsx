068a90400beb40fe0d1c4616e761dbd3b99c7cbe
jmulq <james.mulqueeny@outlook.com>
2023-08-13 14:46:16 +0100

    Final UI improvements

--- packages/nextjs/components/layout.tsx
+++ packages/nextjs/components/layout.tsx
@@ -1,5 +1,6 @@
 // pages/dashboard.js
 import React, { ReactNode } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -8,6 +9,11 @@ const Layout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
   const isLandingPage = router.pathname === "/";
+
+  const isLinkActive = (href: string) => {
+    return router.pathname === href;
+  };
+
   return isLandingPage ? (
     <>{children}</>
   ) : (
@@ -23,27 +29,49 @@ const Layout = ({ children }: { children: ReactNode }) => {
       {/* CONTENT */}
       <div className="flex flex-1">
         {/* SIDEBAR */}
-        <div className="w-1/6 bg-blue-600 text-white">
+        <div className="flex flex-col justify-between w-1/6 bg-blue-600 text-white">
           <nav className="p-4">
             <ul>
               <li className="flex items-center p-2 text-blue-200 mb-4">DASHBOARD</li>
               <li className="mb-2">
-                <Link href="/election" className="flex items-center p-2 hover:bg-blue-400">
+                <Link
+                  href="/election"
+                  className={`flex items-center p-2 hover:bg-blue-400 ${
+                    isLinkActive("/election") ? "bg-blue-400 cursor-default" : ""
+                  }`}
+                >
                   ELECTIONS
                 </Link>
               </li>
               <li className="mb-2">
-                <Link href="/daos" className="flex items-center p-2 hover:bg-blue-400">
+                <Link
+                  href="/daos"
+                  className={`flex items-center p-2 hover:bg-blue-400 ${
+                    isLinkActive("/daos") ? "bg-blue-400 cursor-default" : ""
+                  }`}
+                >
                   DAO HUB
                 </Link>
               </li>
               <li className="mb-2">
-                <Link href="/notifications" className="flex items-center p-2 hover:bg-blue-200">
+                <Link href="/" className="flex items-center p-2 hover:bg-blue-200">
                   NOTIFICATIONS
                 </Link>
               </li>
             </ul>
           </nav>
+          <div className="mb-10">
+            <div className=" text-white mt-10 mb-5">Powered by</div>
+            <div className="flex justify-between p-2 ml-6  mr-3">
+              <Image alt="eth logo" src="/assets/eth_logo.png" width={40} height={30} />
+              <Image className="ml-4" alt="optimism logo" src="/assets/op_logo.svg" width={40} height={30} />
+              <Image alt="worldcoin logo" src="/assets/worldcoin_logo.png" width={70} height={30} />
+            </div>
+            <div className="flex justify-around p-2 ml-8 mr-8">
+              <Image alt="chainlink logo" src="/assets/chainlink_logo.png" width={40} height={20} />
+              <Image alt="graph logo" src="/assets/graph_logo.png" width={40} height={30} />
+            </div>
+          </div>
         </div>
         <div className="p-10 w-5/6 max-h-screen bg-gradient-to-r from-blue-100 to-cyan-100">{children}</div>
       </div>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -5,14 +5,16 @@ import type { NextPage } from "next";
 const Landing: NextPage = () => {
   return (
     <div className="flex justify-center items-center min-h-screen bg-landing-image bg-cover relative">
-      <div className="bg-black bg-opacity-40 rounded-lg max-w-3xl mx-auto text-center p-6">
-        <h1 className="text-6xl font-bold mb-10 text-white">DAOMOCRACY</h1>
-        <p className="text-lg text-white mb-10">
-          DAOmocracy is a thought experiment reimagining the way we practice democracy in society. Through innovative
-          blockchain technology and decentralized decision-making, we aim to create a more inclusive and transparent
-          democratic process, empowering every individual to have a voice in shaping our world.
+      <div className="bg-black bg-opacity-40 rounded-lg max-w-3xl mx-auto text-center p-8">
+        <h1 className="text-6xl font-bold mb-30 text-white">DAOMOCRACY</h1>
+        <p className="text-lg text-white mb-30">
+          DAOmocracy is a thought experiment reimagining the essence of democracy through a DAO of subDAOs structure.
+          Our vision revolves around a dynamic ecosystem where diverse specialized DAOs contribute to various aspects of
+          governance, such as proposing legislation, funding initiatives, and shaping policies. This novel approach
+          would empower individuals to actively engage in shaping our future while fostering transparency, inclusivity,
+          and effective decentralised decision-making.
         </p>
-        <p className="text-xl font-semibold mb-10 text-white">Get involved with your World ID</p>
+        <p className="text-xl font-semibold mb-30 text-white">Get involved with your World ID</p>
         <Link href="/election" className="bg-white text-black px-6 py-3 rounded-md hover:bg-gray-200">
           Start now
         </Link>
